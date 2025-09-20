9dc7512f710dcc9c6f875b5774d3da37a1d84cd1
atul choudhary <atuldagur2009@gmail.com>
2025-09-20 17:55:05 +0530

    Backup of Angular 18 upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,31 +7,23 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PropertyCardComponent } from './components/property-card/property-card.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
 import { UploadPropertyComponent } from './upload-property/upload-property.component';
 import { PropertyPageComponent } from './property-page/property-page.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomePageComponent,
-    PropertyCardComponent,
-    NotFoundComponent,
-    UploadPropertyComponent,
-    PropertyPageComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomePageComponent,
+        PropertyCardComponent,
+        NotFoundComponent,
+        UploadPropertyComponent,
+        PropertyPageComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
